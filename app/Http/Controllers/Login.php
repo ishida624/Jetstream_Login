ee71ccfc74def0e8c3aa3e53d6e0d9fe81b02c64
Gill <ishida624@gmail.com>
2021-01-02 02:14:01 +0800

    update Login.php ,if no account register new user

--- app/Http/Controllers/Login.php
+++ app/Http/Controllers/Login.php
@@ -4,24 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class Login extends Controller
 {
     //
-
     public function authenticate(Request $request)
     {
         $credentials = $request->only('email', 'password');
-
+        # 帳密驗證成功
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
             return redirect()->intended('dashboard');
-            // return 'login success';
+        } else {
+            # 若是沒有該帳號 直接註冊
+            if (!User::where('email', $credentials['email'])->first()) {
+                User::create([
+                    'name' => 'new_user',
+                    'email' => $credentials['email'],
+                    'password' => Hash::make($credentials['password'])
+                ]);
+                $request->session()->regenerate();
+                return redirect()->intended('dashboard');
+            }
+            # 有此帳號 但密碼錯誤
+            return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+            ]);
         }
-
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ]);
     }
 }
